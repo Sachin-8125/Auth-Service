c619d4ce78d121c17fda179ffa569b3a1aa95901
Sachin-8125 <devinewolf7@gmail.com>
2024-08-20 19:34:13 +0530

    Added user isAutenticated function

--- src/services/user-service.js
+++ src/services/user-service.js
@@ -40,6 +40,23 @@ class UserService{
         }
     }
 
+    async isAuthenticated(token){
+        try {
+            const isTokenVerified = this.verifyToken(token);
+            if(!isTokenVerified){
+                throw{error: 'Invalid token'};
+            }
+            const user = this.userRepository.getById(response.id);
+            if(!user){
+                throw{error:"No user with the coresponding token exists"};
+            }
+            return user.id;
+        } catch (error) {
+            console.log("something wnet wrong in auth process");
+            throw error;
+        }
+    }
+
     createToken(user) {
         try {
             const result = jwt.sign(user,JWT_KEY,{expiresIn:'1h'});
